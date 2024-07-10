a675dcc3af436d0edca4fc3f46da91ec754bc098
Souvik Ojha <80455812+techsouvik@users.noreply.github.com>
2024-07-10 10:46:50 +0530

    creds updated

--- deploy_service/src/aws.ts
+++ deploy_service/src/aws.ts
@@ -4,9 +4,9 @@ import fs from "fs";
 import path from "path";
 
 const s3 = new S3({
-    accessKeyId: "6d77e0efbc785f4b94d756f91c9b368a" || process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: "f5fa9c62b0a261b503e700c4e597a2a9c38624302a47a49ad6c3de408444431f" || process.env.AWS_SECRET_ACCESS_KEY,
-    endpoint: "https://f584cf9eb315183572305333890e54f2.r2.cloudflarestorage.com" || process.env.AWS_S3_ENDPOINT
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: process.env.AWS_S3_ENDPOINT
 })
 
 
--- upload_service/src/aws.ts
+++ upload_service/src/aws.ts
@@ -3,9 +3,9 @@ import { S3 } from "aws-sdk"
 import fs from "fs"
 
 const s3 = new S3({
-    accessKeyId: "6d77e0efbc785f4b94d756f91c9b368a" || process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: "f5fa9c62b0a261b503e700c4e597a2a9c38624302a47a49ad6c3de408444431f" || process.env.AWS_SECRET_ACCESS_KEY,
-    endpoint: "https://f584cf9eb315183572305333890e54f2.r2.cloudflarestorage.com" || process.env.AWS_S3_ENDPOINT
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: process.env.AWS_S3_ENDPOINT
 })
 
 
